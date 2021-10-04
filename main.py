ae8391875e3a947a81b84ec5cd5120af593eadb9
mpiotrowski-ias <mpiotrowski@integralads.com>
2021-10-04 19:21:10 +0200

    Add predicion code

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import t5
 import tensorflow.compat.v1 as tf
 from omegaconf import DictConfig, OmegaConf
 import t5.models
-from task_registry import register_datasets
+from task_registry import register_datasets, DEFAULT_VOCAB
 
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
 
 TPU_TOPOLOGY = "v3-8"
 
@@ -37,11 +41,35 @@ def run_training(tpu_address, config):
         **config.model,
     )
 
-    model.finetune(
-        mixture_or_task_name=config.train_task,
-        pretrained_model_dir=config.pretrained_dir,
-        finetune_steps=config.finetune_steps,
-    )
+    org_ckpt = t5.models.utils.get_latest_checkpoint_from_dir(config.pretrained_dir)
+    try:
+        last_ckpt = t5.models.utils.get_latest_checkpoint_from_dir(config.model_dir)
+    except ValueError:
+        last_ckpt = org_ckpt
+
+    if last_ckpt < org_ckpt + config.finetune_steps:
+        model.finetune(
+            mixture_or_task_name=config.train_task,
+            pretrained_model_dir=config.pretrained_dir,
+            finetune_steps=config.finetune_steps,
+        )
+    else:
+        logger.info("Finetuning already completed, skipping")
+
+    for predict_file in config.predict_files:
+        logger.info("Predicting file %s", predict_file)
+
+        ts = datetime.utcnow().strftime("%Y%m%d")
+        suffix = config.model_dir.strip("/").split("/")[-1]
+        outfile = predict_file + f"-{suffix}-{ts}"
+        logger.info("Writing to %s", outfile)
+
+        model.predict(
+            predict_file,
+            outfile,
+            vocabulary=DEFAULT_VOCAB,
+            checkpoint_steps="all",
+        )
 
 
 @hydra.main(config_path="config", config_name="config")
